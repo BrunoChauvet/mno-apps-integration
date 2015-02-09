369d0615fec47b2db4ac2a138bcc260e34bf3847
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-02-09 19:40:14 +1100

    Orange DB cleanup

--- orangehrm/reset_database.sql
+++ orangehrm/reset_database.sql
@@ -1,3 +1,8 @@
 DELETE FROM mno_id_map;
+UPDATE ohrm_organization_gen_info SET name = null, tax_id = null, registration_number = null, phone = null, fax = null, email = null, country = null, province = null, city = null, zip_code = null, street1 = null, street2 = null, note = null;
 DELETE FROM hs_hr_employee;
 DELETE FROM ohrm_job_title;
+DELETE FROM ohrm_location;
+DELETE FROM ohrm_customer;
+DELETE FROM ohrm_project;
+DELETE FROM ohrm_project_activity;
