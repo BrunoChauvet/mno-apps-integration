2040f42032c96b61ac07f4f77cf943cf087efd91
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-03-05 00:10:25 +1100

    Update cucumber steps

--- cucumber/features/step_definitions/connec/api_steps.rb
+++ cucumber/features/step_definitions/connec/api_steps.rb
@@ -10,15 +10,18 @@ end
 
 Then(/^Connec should contain the Organizations$/) do |table|
   client = Connec::ClientV1.new
-  organizations = client.organizations
+  
+  eventually {
+    organizations = client.organizations
 
-  table.hashes.each do |row|
-    matching_organization = organizations.select do |organization|
-      organization['name'] == row['Name']
+    table.hashes.each do |row|
+      matching_organization = organizations.select do |organization|
+        organization['name'] == row['Name']
+      end.first
+
+      raise "Organization not found: #{row}" unless matching_organization
     end
-
-    raise "Organization not found: #{row}" unless matching_organization
-  end
+  }
 end
 
 Given(/^Connec contains the Contacts$/) do |table|
@@ -28,16 +31,19 @@ end
 
 Then(/^Connec should contain the Contacts$/) do |table|
   client = Connec::ClientV1.new
-  people = client.people
+  
+  eventually {
+    people = client.people
 
-  table.hashes.each do |row|
-    matching_contact = people.select do |person|
-      person['name']['givenNames'] == row['First name']
-      person['name']['familyName'] == row['Last name']
+    table.hashes.each do |row|
+      matching_contact = people.select do |person|
+        person['name']['givenNames'] == row['First name']
+        person['name']['familyName'] == row['Last name']
+      end.first
+
+      raise "Person not found: #{row}" unless matching_contact
     end
-
-    raise "Person not found: #{row}" unless matching_contact
-  end
+  }
 end
 
 Given(/^Connec contains the Accounts$/) do |table|
@@ -47,21 +53,24 @@ end
 
 Then(/^Connec should contain the Accounts$/) do |table|
   client = Connec::ClientV1.new
-  accounts = client.accounts
+  
+  eventually {
+    accounts = client.accounts
 
-  table.hashes.each do |row|
-    matching_account = accounts.select do |account|
-      account['name'] == row['Name']
-      account['code'] == row['Code']
-      account['description'] == row['Description']
-      account['currency'] == row['Currency']
-      account['classification'] == row['Classification']
-      account['type'] == row['Type']
-      account['subType'] == row['SubType']
+    table.hashes.each do |row|
+      matching_account = accounts.select do |account|
+        account['name'] == row['Name']
+        account['code'] == row['Code']
+        account['description'] == row['Description']
+        account['currency'] == row['Currency']
+        account['classification'] == row['Classification']
+        account['type'] == row['Type']
+        account['subType'] == row['SubType']
+      end.first
+
+      raise "Account not found: #{row}" unless matching_account
     end
-
-    raise "Account not found: #{row}" unless matching_account
-  end
+  }
 end
 
 Given(/^Connec contains the Tax Codes$/) do |table|
@@ -71,16 +80,19 @@ end
 
 Then(/^Connec should contain the Tax Codes$/) do |table|
   client = Connec::ClientV1.new
-  tax_codes = client.tax_codes
+  
+  eventually {
+    tax_codes = client.tax_codes
 
-  table.hashes.each do |row|
-    matching_tax_code = tax_codes.select do |item|
-      item['name'] == row['Tax Code name']
-      item['description'] == row['Tax Code description']
+    table.hashes.each do |row|
+      matching_tax_code = tax_codes.select do |item|
+        item['name'] == row['Tax Code name']
+        item['description'] == row['Tax Code description']
+      end.first
+
+      raise "Tax Code not found: #{row}" unless matching_tax_code
     end
-
-    raise "Tax Code not found: #{row}" unless matching_tax_code
-  end
+  }
 end
 
 Given(/^Connec contains the Items$/) do |table|
@@ -90,16 +102,19 @@ end
 
 Then(/^Connec should contain the Items$/) do |table|
   client = Connec::ClientV1.new
-  items = client.items
+  
+  eventually {
+    items = client.items
 
-  table.hashes.each do |row|
-    matching_item = items.select do |item|
-      item['name'] == row['Product name']
-      item['sale']['price'] == row['Product price']
+    table.hashes.each do |row|
+      matching_item = items.select do |item|
+        item['name'] == row['Product name']
+        item['sale']['price'] == row['Product price']
+      end.first
+
+      raise "Item not found: #{row}" unless matching_item
     end
-
-    raise "Item not found: #{row}" unless matching_item
-  end
+  }
 end
 
 Given(/^Connec contains the Invoices$/) do |table|
@@ -115,36 +130,41 @@ end
 
 Then(/^Connec should contain the Invoices$/) do |table|
   client = Connec::ClientV1.new
-  invoices = client.invoices
+  
+  eventually {
+    invoices = client.invoices
 
-  table.hashes.each do |row|
-    matching_invoice = invoices.select do |invoice|
-      invoice['transactionNumber'] == row['Number']
-      Time.at(invoice['transactionDate']).strftime('%F') == row['Date']
-      Time.at(invoice['dueDate']).strftime('%F') == row['Due']
-      invoice['status'] == row['Status']
-      invoice['amount']['price'].to_s == row['Total amount']
+    table.hashes.each do |row|
+      matching_invoice = invoices.select do |invoice|
+        invoice['transactionNumber'] == row['Number']
+        Time.at(invoice['transactionDate']).strftime('%F') == row['Date']
+        Time.at(invoice['dueDate']).strftime('%F') == row['Due']
+        invoice['status'] == row['Status']
+        invoice['amount']['price'].to_s == row['Total amount']
+      end.first
+      raise "Invoice not found: #{row}" unless matching_invoice
     end
-
-    raise "Invoice not found: #{row}" unless matching_invoice
-  end
+  }
 end
 
 Then(/^Connec should contain only the Invoices$/) do |table|
   client = Connec::ClientV1.new
-  invoices = client.invoices
+  
+  eventually {
+    invoices = client.invoices
 
-  actual_invoices = []
-  actual_invoices << ['Number', 'Date', 'Due', 'Status', 'Total amount']
-  invoices.each { |invoice| actual_invoices << [
-    invoice['transactionNumber'],
-    Time.at(invoice['transactionDate']).strftime('%F'),
-    Time.at(invoice['dueDate']).strftime('%F'),
-    invoice['status'],
-    invoice['amount']['price'].to_s
-  ] }
+    actual_invoices = []
+    actual_invoices << ['Number', 'Date', 'Due', 'Status', 'Total amount']
+    invoices.each { |invoice| actual_invoices << [
+      invoice['transactionNumber'],
+      Time.at(invoice['transactionDate']).strftime('%F'),
+      Time.at(invoice['dueDate']).strftime('%F'),
+      invoice['status'],
+      invoice['amount']['price'].to_s
+    ] }
 
-  table.diff! actual_invoices
+    table.diff! actual_invoices
+  }
 end
 
 Given(/^Connec contains the Employees$/) do |table|
@@ -154,27 +174,29 @@ end
 
 Then(/^Connec should contain the Employees$/) do |table|
   client = Connec::ClientV1.new
-  employees = client.employees
+  eventually {
+    employees = client.employees
 
-  table.hashes.each do |row|
-    matching_employee = employees.select do |employee|
-      employee['name']['givenNames'] == row['First name']
-      employee['name']['familyName'] == row['Last name']
-      employee['employeeId'] == row['Employee ID']
+    table.hashes.each do |row|
+      matching_employee = employees.select do |employee|
+        employee['name']['givenNames'] == row['First name']
+        employee['name']['familyName'] == row['Last name']
+        employee['employeeId'] == row['Employee ID']
 
-      employee['contacts']['address']['postalAddress']['streetAddress'] == row['Address Street 1']
-      employee['contacts']['address']['postalAddress']['streetAddress2'] == row['Address Street 2']
-      employee['contacts']['address']['postalAddress']['locality'] == row['City']
-      employee['contacts']['address']['postalAddress']['region'] == row['State']
-      employee['contacts']['address']['postalAddress']['country'] == row['Country']
+        employee['contacts']['address']['postalAddress']['streetAddress'] == row['Address Street 1']
+        employee['contacts']['address']['postalAddress']['streetAddress2'] == row['Address Street 2']
+        employee['contacts']['address']['postalAddress']['locality'] == row['City']
+        employee['contacts']['address']['postalAddress']['region'] == row['State']
+        employee['contacts']['address']['postalAddress']['country'] == row['Country']
 
-      employee['contacts']['email']['address'] == row['Work Email']
-      employee['contacts']['email']['address2'] == row['Other Email']
+        employee['contacts']['email']['address'] == row['Work Email']
+        employee['contacts']['email']['address2'] == row['Other Email']
 
-      employee['contacts']['telephone']['voice'] == row['Home Telephone']
-      employee['contacts']['telephone']['voice2'] == row['Work Telephone']
-      employee['contacts']['telephone']['mobile'] == row['Mobile']
+        employee['contacts']['telephone']['voice'] == row['Home Telephone']
+        employee['contacts']['telephone']['voice2'] == row['Work Telephone']
+        employee['contacts']['telephone']['mobile'] == row['Mobile']
+      end.first
+      raise "Employee not found: #{row}" unless matching_employee
     end
-    raise "Employee not found: #{row}" unless matching_employee
-  end
+  }
 end
