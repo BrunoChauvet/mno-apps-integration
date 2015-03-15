77cd0ce35afd2b02dc13b9581e3a99e829672418
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-03-16 09:04:06 +1100

    delete punch

--- timetrex/reset_database.sql
+++ timetrex/reset_database.sql
@@ -1,5 +1,6 @@
 DELETE FROM mno_id_map;
 DELETE FROM system_log;
+DELETE FROM system_log_detail;
 DELETE FROM users;
 DELETE FROM permission_user;
 DELETE FROM user_preference;
@@ -10,3 +11,7 @@ DELETE FROM punch_control;
 DELETE FROM punch;
 DELETE FROM pay_period_schedule;
 DELETE FROM pay_period;
+DELETE FROM pay_stub_entry_account WHERE id > 75;
+DELETE FROM pay_code WHERE id > 32;
+DELETE FROM user_date;
+DELETE FROM user_date_total;
