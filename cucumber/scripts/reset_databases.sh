17b1cc7e0b90e860b9a41e062ba6fcdedf110b72
BrunoChauvet <bruno.chauvet@live.fr>
2015-03-27 14:50:11 +1100

    Add vtiger6 DB reset

--- cucumber/scripts/reset_databases.sh
+++ cucumber/scripts/reset_databases.sh
@@ -2,6 +2,7 @@
 mysql dolibarr -udolibarr -pdolibarr < ../dolibarr/reset_database.sql
 mysql simpleinvoices -usimpleinvoices -psimpleinvoices < ../simpleinvoices/reset_database.sql
 mysql vtiger -uvtiger -pvtiger < ../vtiger/reset_database.sql
+mysql vtiger6 -uvtiger6 -pvtiger6 < ../vtiger6/reset_database.sql
 mysql sugarcrm -usugarcrm -psugarcrm < ../sugarcrm/reset_database.sql
 mysql limesurvey -ulimesurvey -plimesurvey < ../limesurvey/reset_database.sql
 mysql orangehrm -uorangehrm -porangehrm < ../orangehrm/reset_database.sql
--- vtiger/reset_vtiger.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Reset vTiger installation
-mysql vtiger -uvtiger -pvtiger -e 'drop database vtiger; create database vtiger;'
-chmod 777 config.inc.php tabdata.php parent_tabdata.php
