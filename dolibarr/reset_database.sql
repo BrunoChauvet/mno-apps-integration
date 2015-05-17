91406654707f9126ac33fd47288a42fed6343ee4
BrunoChauvet <bruno.chauvet@live.fr>
2015-05-17 18:30:05 +1000

    Update reset scripts

--- dolibarr/reset_database.sql
+++ dolibarr/reset_database.sql
@@ -21,5 +21,7 @@ DELETE FROM llx_bank_account;
 DELETE FROM llx_c_tva WHERE rowid >= 2469;
 DELETE FROM llx_paiement;
 DELETE FROM llx_paiement_facture;
+DELETE FROM llx_paiementfourn;
+DELETE FROM llx_paiementfourn_facturefourn;
 DELETE FROM llx_user_rights;
 DELETE FROM llx_user;
